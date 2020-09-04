b88091ba729c7a866149ad7aa58e1b1a5c00aebd
Jakub Pavlik <severus@post.cz>
2020-09-04 22:44:08 +0200

    initial commit

---
+++ extension.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var findLinkByTitle = function (title) {
+  return document
+    .evaluate("//a[@title = '"+title+"']", document, null, XPathResult.ANY_TYPE, null)
+    .iterateNext();
+};
+
+// TODO: this probably works only with locale EN.
+var previous = function () {
+    findLinkByTitle('Browse to previous page/image.').click();
+};
+
+var next = function () {
+    findLinkByTitle('Browse to next page/image.').click();
+};
+
+var close = function () {
+    findLinkByTitle('Close floating window.').click();
+};
+
+var maximize = function () {
+    findLinkByTitle('Maximize floating window.').click();
+};
+
+document.addEventListener('keydown', (event) => {
+  const keyName = event.key;
+
+  if (keyName === 'ArrowLeft') {
+    previous();
+  } else if (keyName === 'ArrowRight') {
+    next();
+  } else if (keyName === 'Escape') {
+    close();
+  } else if (keyName === 'f' || keyName === 'ArrowUp') {
+    maximize();
+  }
+}, false);
